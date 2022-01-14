5fd477019a675bdb502e14934b30abced04e6218
bazukax <33648731+bazukax@users.noreply.github.com>
2022-01-14 17:46:27 +0200

    Landscape generated with procedural generation, very basic

---
+++ Assets/Scripts/GenerateScene.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateScene : MonoBehaviour
+{
+   
+    public static int mapX=100;
+    public static int mapZ=100;
+
+    public float[,] mapArray = new float[mapX,mapZ];
+
+    public GameObject cube;
+    public GameObject grass;
+
+    void Start()
+    {
+        for(int x = 0; x<mapX;x++)
+        {
+            for(int z = 0; z<mapX;z++)
+            {
+                    mapArray[x,z] = Mathf.PerlinNoise(x*0.03f,z*0.03f) *10;
+                   // Instantiate(cube, new Vector3(x, mapArray[x,z], z), Quaternion.identity);
+                    Instantiate(grass, new Vector3(x, mapArray[x,z]+1, z), Quaternion.identity);
+                    
+            }
+        }
+
+       
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
